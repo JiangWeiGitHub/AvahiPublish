86da83a7c0cf66448e43e5e903dc0a8509d59bab
John <wei.jiang@winsuntech.cn>
2016-05-04 18:25:24 +0800

    Update avahiPublish.js

--- avahiPublish.js
+++ avahiPublish.js
@@ -4,7 +4,7 @@ var sys = require('util')
 var exec = require('child_process').exec;
 var child;
 
-child = exec("avahi-publish-service 'Wisnuc AppStation' _http._tcp 6666 'Model=Generic X86-64 Mac=12:34:56:78:90'", function (error, stdout, stderr) {
+child = exec("avahi-publish-service Wisnuc_AppStation _http._tcp 6666 Model=Generic_X86-64 Mac=12:34:56:78:90", function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     if (error !== null) {
         console.log('exec error: ' + error);
