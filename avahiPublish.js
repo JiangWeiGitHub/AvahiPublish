71b5c7d5ebab39721963bd0786794c350f6dc0c7
John <wei.jiang@winsuntech.cn>
2016-05-05 11:30:18 +0800

    Update avahiPublish.js

--- avahiPublish.js
+++ avahiPublish.js
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 
-var sys = require('util')
-var exec = require('child_process').exec;
-var child;
+var sys = require('util');
+var exec = require('child_process').execSync;
 
-child = exec("avahi-publish-service 'Wisnuc AppStation' _http._tcp 6666 Model=Generic_X86-64 Mac=12:34:56:78:90", function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    if (error !== null) {
-        console.log('exec error: ' + error);
-    }
-});
+var platform_cmd = exec("uname -i");
+var platform_value = platform_cmd.toString().replace(/\r|\n|\r\n/g,"");
+
+var mac_cmd = exec("cat /sys/class/net/enp2s0/address");
+var mac_value = mac_cmd.toString().replace(/\r|\n|\r\n/g,"");
+
+var cache = [];
+cache.push("avahi-publish-service 'Wisnuc AppStation' _http._tcp 6666 Model=");
+cache.push(platform_value);
+cache.push(' Mac=');
+cache.push(mac_cmd);
+var result = cache.join('');
+console.log('result: ' + result);
+
+var avahi_cmd = exec(result);
