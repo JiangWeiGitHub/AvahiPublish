18356fcee3f3b558c21acbc0b47e74171e07d8ec
John <wei.jiang@winsuntech.cn>
2016-06-17 16:17:06 +0800

    Update avahi-publish.js

--- avahi-publish.js
+++ avahi-publish.js
@@ -9,7 +9,7 @@ var platform_value = platform_cmd.toString().replace(/\r|\n|\r\n/g,"");
 var mac_cmd = exec("cat /sys/class/net/enp2s0/address");
 var mac_value = mac_cmd.toString().replace(/\r|\n|\r\n/g,"");
 
-var result = "avahi-publish-service 'Wisnuc AppStation' _http._tcp 6666 Model=" + platform_value + " Mac=" +  mac_cmd + "&";
+var result = "avahi-publish-service 'Wisnuc AppStation' _http._tcp 6666 Model=" + platform_value + " Mac=" +  mac_value + "&";
 //console.log('result: ' + result);
 
 var avahi_cmd = exec(result);
